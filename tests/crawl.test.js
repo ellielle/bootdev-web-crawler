d46eb87cd94fc0c3a7abaf2d53ceafd117e551d2
ellielle <40385743+ellielle@users.noreply.github.com>
2024-01-25 08:05:55 -0500

    update tests

--- tests/crawl.test.js
+++ tests/crawl.test.js
@@ -1,5 +1,5 @@
 const { describe, test, expect } = require("@jest/globals");
-const { normalizeUrl, getURLsFromHTML } = require("../crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("../crawl.js");
 const fs = require("fs");
 const path = require("path");
 const htmlDoc = fs.readFileSync(
@@ -15,12 +15,12 @@ describe("noramilzeUrl function", () => {
       "http://blog.boot.dev/path",
     ];
     URLS.forEach((url) => {
-      expect(normalizeUrl(url)).toBe("blog.boot.dev/path");
+      expect(normalizeURL(url)).toBe("blog.boot.dev/path");
     });
   });
   test("it throws an error with invalid URLs", () => {
     const url = "//blog.boot.dev/path";
-    expect(() => normalizeUrl(url)).toThrow(Error);
+    expect(() => normalizeURL(url)).toThrow(Error);
   });
 });
 
